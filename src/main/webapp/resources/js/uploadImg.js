f7f0f204fa0935b144d1c8f01d37ce34c81ef6cb
Alessandro Menezes <alessandroadm@live.com>
2019-10-16 20:19:40 -0300

    alterações na validação da imagem de produtos e cestas no cadastro

--- src/main/webapp/resources/js/uploadImg.js
+++ src/main/webapp/resources/js/uploadImg.js
@@ -4,14 +4,22 @@ var $fotoPreview = document.getElementById("foto-preview");
 
 
 function exibirImagem(imagem) {
-    console.log(imagem[0].target);
+    var tamanoImagemMB = parseFloat(imagem[0].size) / 1024;
+    
+    // Tratar o tamanho da imagem
+    if(tamanoImagemMB >= 60){
+	console.log(tamanoImagemMB);
+	alert("Imagem maior que o tamanho permitido,\n tente com uma imagem menor 200 x 200px");
+    }else{  
     if (imagem && imagem[0]) {
 	var reader = new FileReader();
 	reader.onload = function(e) { 
 	    document.getElementById("foto-preview").src = e.target.result;
 	};
 	reader.readAsDataURL(imagem[0]);
+	
     }
+}
 }
 
 function teste(e) {
