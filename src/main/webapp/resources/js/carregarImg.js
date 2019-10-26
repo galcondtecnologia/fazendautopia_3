ae796280325bfdb135fdb12dbfca6a385d841173
Alessandro Menezes <alessandroadm@live.com>
2019-10-26 09:43:23 -0300

    carregar imagem ao editar

---
+++ src/main/webapp/resources/js/carregarImg.js
@@ -0,0 +1,18 @@
+var itens = document.querySelectorAll("#tabela_data tr");
+var icon = document.getElementById("foto-preview");
+var item;
+
+
+    for (var i = 0; itens.length > i; i++) {
+	itens[i].addEventListener('click', function(e) {
+	   item = e.path[i];
+	   var image = item.querySelector("td img");
+	   checkImg(image);
+	});
+    }
+
+function checkImg(image) {
+    setTimeout(() => {
+	icon.src = image.src;	
+    }, 200);
+}
--- src/main/webapp/resources/js/uploadImg.js
+++ src/main/webapp/resources/js/uploadImg.js
@@ -1,28 +1,27 @@
 var $btnUploadImg = document.getElementById("foto");
 var $fotoPreview = document.getElementById("foto-preview");
 
-
-
 function exibirImagem(imagem) {
     var tamanoImagemMB = parseFloat(imagem[0].size) / 1024;
-    
+
     // Tratar o tamanho da imagem
-    if(tamanoImagemMB >= 60){
+    if (tamanoImagemMB >= 60) {
 	alert("Imagem maior que o tamanho permitido,\n tente com uma imagem menor 200 x 200px");
-    }else if(imagem[0].type != "image/png"){
+    } else if (imagem[0].type != "image/png") {
 	alert("Selecione uma imagem em formato PNG");
-    }else{  
-    if (imagem && imagem[0]) {
-	var reader = new FileReader();
-	reader.onload = function(e) { 
-	    document.getElementById("foto-preview").src = e.target.result;
-	};
-	reader.readAsDataURL(imagem[0]);
-	
+    } else {
+	if (imagem && imagem[0]) {
+	    var reader = new FileReader();
+	    reader.onload = function(e) {
+		document.getElementById("foto-preview").src = e.target.result;
+	    };
+	    reader.readAsDataURL(imagem[0]);
+
+	}
     }
-}
 }
 
 function teste(e) {
     exibirImagem(e.files);
 }
+
