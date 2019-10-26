b1b8d23bc9ec1996a50486a91f797310ff447589
Alessandro Menezes <alessandroadm@live.com>
2019-10-26 20:33:25 -0300

    Ajuste da imagem quando vazia

--- src/main/webapp/resources/js/carregarImg.js
+++ src/main/webapp/resources/js/carregarImg.js
@@ -4,20 +4,20 @@ var listBtnEditar = document.querySelectorAll("button");
 var imagemBean = document.querySelector("#foto");
 var imagemBase64 = '';
 
-
-
 for (var i = 0; itens.length > i; i++) {
     itens[i].addEventListener('click', function(e) {
 	var itemSelecionado = e.currentTarget;
 	var image = itemSelecionado.querySelector("td img");
 	imagemBase64 = image.src;
-	}, false);
-    
-    }
+    }, false);
 
-function loadImg() {
-	document.getElementById("foto-preview").src = imagemBase64;	
-	console.log(imagemBean);
 }
 
+function loadImg() {
+    if (imagemBase64 !== "") {
+	document.getElementById("foto-preview").src = imagemBase64;
+    } else {
+	document.getElementById("foto-preview").src = "../../resources/images/icons/SemImagem-300x300px.png";
+    }
 
+}
