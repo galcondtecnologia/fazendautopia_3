1d74e132ccc57c1df8880ff6f52613ef2ce82ef0
Alessandro Menezes <alessandroadm@live.com>
2019-10-29 23:40:39 -0300

    alteração no dialog da cesta selecionada na pagina index

--- src/main/webapp/resources/js/dialogIndex.js
+++ src/main/webapp/resources/js/dialogIndex.js
@@ -1,8 +1,13 @@
-function capturarProdutosDaLista(){
+var qtdAlteracoesPermitidas = 2;
+var qtdProdutosRetirados = 0;
+var alerta = document.querySelector("#dialogCestaAlerta");
+
+
+function capturarProdutosDaLista() {
     var lista = document.querySelector("#produtosList");
     var arr = lista.querySelectorAll("li .remove-icon");
     var i = 0;
-    while(arr[i]){
+    while (arr[i]) {
 	arr[i].addEventListener('click', remover);
 	console.log(i);
 	i++;
@@ -11,11 +16,20 @@ function capturarProdutosDaLista(){
 
 function remover(e) {
     var item = e.path[10];
-    somarMultiplicador(item);
-    console.log(item);
-    item.parentNode.removeChild(item);
-    botao.style.display='block';
+    var saldoRetiradas = somarMultiplicador(item);
 
+    item.parentNode.removeChild(item);
+    
+    botao.style.display = 'block';
+    let _lista = document.querySelector("#produtosList");
+    let _iconesRemover = _lista.querySelectorAll("li .remove-icon");
+    
+    if (qtdProdutosRetirados == qtdAlteracoesPermitidas) {
+	for (var indice = 0; indice < _iconesRemover.length; indice++) {
+	    _iconesRemover[indice].style.visibility = "hidden";
+	}
+	alerta.style.display = 'block';
+    }
 };
 var botao = document.querySelector("#botaoAddProduto");
 var multiplicador = 0;
@@ -23,5 +37,15 @@ var multiplicador = 0;
 function somarMultiplicador(_item) {
     var pesoMultiplicador = _item.querySelector('.multiplicador').textContent;
     multiplicador = multiplicador + parseInt(pesoMultiplicador);
-    console.log('multiplicador = ',multiplicador);
+    console.log('multiplicador = ', multiplicador);
+    
+    qtdProdutosRetirados = qtdProdutosRetirados + 1;
+}
+
+function ocultarIconRemove(icons) {
+    icons.style.display = 'none';
+}
+
+function fecharAlerta() {
+    alerta.style.display = 'none';
 }
