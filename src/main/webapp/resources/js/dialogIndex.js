949ba7b95387b2aef957dc530b2b28331616b8da
Alessandro Menezes <alessandroadm@live.com>
2019-10-31 22:39:27 -0300

    regras de personalização de cestas

--- src/main/webapp/resources/js/dialogIndex.js
+++ src/main/webapp/resources/js/dialogIndex.js
@@ -1,10 +1,11 @@
 var qtdAlteracoesPermitidas = 2;
-var qtdProdutosRetirados = 0;
-var alerta = document.querySelector("#dialogCestaAlerta");
 
+var qtdProdutosRetirados = 0;
+var qtdProdutosInseridos = 0;
+var alerta = document.querySelector("#dialogCestaAlerta");
+var lista = document.querySelector("#produtosList ul");
 
 function capturarProdutosDaLista() {
-    var lista = document.querySelector("#produtosList");
     var arr = lista.querySelectorAll("li .remove-icon");
     var i = 0;
     while (arr[i]) {
@@ -19,16 +20,16 @@ function remover(e) {
     var saldoRetiradas = somarMultiplicador(item);
 
     item.parentNode.removeChild(item);
-    
+
     botao.style.display = 'block';
     let _lista = document.querySelector("#produtosList");
     let _iconesRemover = _lista.querySelectorAll("li .remove-icon");
-    
+
     if (qtdProdutosRetirados == qtdAlteracoesPermitidas) {
 	for (var indice = 0; indice < _iconesRemover.length; indice++) {
 	    _iconesRemover[indice].style.visibility = "hidden";
 	}
-	alerta.style.display = 'block';
+
     }
 };
 var botao = document.querySelector("#botaoAddProduto");
@@ -38,7 +39,6 @@ function somarMultiplicador(_item) {
     var pesoMultiplicador = _item.querySelector('.multiplicador').textContent;
     multiplicador = multiplicador + parseInt(pesoMultiplicador);
     console.log('multiplicador = ', multiplicador);
-    
     qtdProdutosRetirados = qtdProdutosRetirados + 1;
 }
 
@@ -49,3 +49,32 @@ function ocultarIconRemove(icons) {
 function fecharAlerta() {
     alerta.style.display = 'none';
 }
+
+function capturarProdudoAdicional(e) {
+    console.log(lista);
+    if (qtdProdutosInseridos < qtdAlteracoesPermitidas
+	    && qtdProdutosInseridos < qtdProdutosRetirados) {
+	lista.appendChild(e.currentTarget);
+	qtdProdutosInseridos = qtdProdutosInseridos + 1;
+	if (qtdProdutosInseridos == qtdAlteracoesPermitidas) {
+	    botao.style.display = 'none';
+	    alerta.style.display = 'block';
+	}
+    } else {
+	botao.style.display = 'none';
+	alerta.style.display = 'block';
+    }
+}
+
+var dataProdutosSelecao = document.querySelector("#data-produtos-selecao");
+
+function load() {
+    const itens = dataProdutosSelecao.querySelectorAll(".ui-datascroller-item");
+    let ne = 0;
+    while (itens[ne]) {
+	itens[ne].addEventListener('click', capturarProdudoAdicional);
+	ne++;
+    }
+}
+
+load();
