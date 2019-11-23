068a295c4a355366c429651609bb244429ebecac
Alessandro Menezes <alessandroadm@live.com>
2019-11-23 17:31:15 -0300

    envio do pedido via JSON

---
+++ src/main/webapp/resources/js/post.js
@@ -0,0 +1,66 @@
+const form = document.querySelector('#formulario');
+let pedido = new Object(null);
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  criarPedido(form);
+  enviarParaServidor();
+});
+
+
+function enviarParaServidor() {
+    fetch('https://reqres.in/api/users', {
+	    method: 'POST',
+	    body: JSON.stringify(pedido)
+	  })
+	    .then(function (response) {
+	      return response.json();
+	    })
+	    .then(function (response) {
+	      console.log(response);
+	      alert('Resposta de sucesso do servidor: ' + JSON.stringify(response));
+	    });
+}
+
+
+
+function criarPedido(form) {
+   
+    let inputs = form.querySelectorAll('input');
+    for (let input of inputs) {
+	switch (input.type) {
+        case "checkbox":
+          if (input.checked) {
+            pedido[input.name] = 'Sim';
+          } else {
+            pedido[input.name] = 'Nao';
+          }
+          break
+        case "radio":
+          if (input.checked) {
+            pedido[input.name] = input.value;
+          }
+          break
+        case "text":
+          pedido[input.name] = input.value;
+          break;
+        case "number":
+          pedido[input.name] = input.value;
+          break;
+        default:
+      }
+    }
+    console.log(pedido);
+    console.log(JSON.stringify(pedido));
+  }
+criarPedido(form);
+
+
+
+
+
+
+
+
+
+
